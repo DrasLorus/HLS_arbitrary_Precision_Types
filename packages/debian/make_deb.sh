084b5bbd8bc6c32758e4a44c646b1ee53a252e22
Camille Monière <camillemvdd@gmail.com>
2022-02-14 12:06:14 +0100

    Update package helpers

--- packages/debian/make_deb.sh
+++ packages/debian/make_deb.sh
@@ -1,8 +1,30 @@
 #! /bin/sh
-version="$(grep "Version" control | cut -d ' ' -f 2)"
 
-mkdir -p hls-ap-types_"$version"-1_all/usr/include/ap_types
-cp -r ../../include hls-ap-types_"$version"-1_all/usr/include/ap_types
+# 
+# Copyright 2022 DrasLorus.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+
+version="$(grep "Version" control | cut -d ' ' -f 2)"
+deb_dir="hls-ap-types_$version-1_all"
+include_dir="$deb_dir/usr/include"
+rm -rfv "$deb_dir" 
+
+install -dm755 "$include_dir"/ap_types/etc
+install -m644 -t "$include_dir"/ap_types ../../include/*.h 
+install -Dm644 -t "$include_dir"/ap_types/etc ../../include/etc/ap_private.h
+install -m644 -t "$include_dir"/ ../ap_*.h 
 
 mkdir -p hls-ap-types_"$version"-1_all/DEBIAN
 cp control hls-ap-types_"$version"-1_all/DEBIAN
